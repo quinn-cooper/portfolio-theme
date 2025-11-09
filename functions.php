10efb11b2cb284f2b39b1183fb8e77a8f9b2514d
Quinn Cooper <quinncooper88@gmail.com>
2025-11-09 00:15:38 -0800

    Added cpt.php to change menu labels from Posts to Projects, enqueued in functions.php.

--- functions.php
+++ functions.php
@@ -2,11 +2,27 @@
 
 // Enqueue CSS files
 function portfoliotheme_enqueue_styles() {
-    // 1. Normalize CSS
-    wp_enqueue_style('normalize', get_template_directory_uri() . '/css/normalize.css');
+    
+    // 1. Theme stylesheet
+    wp_enqueue_style( 
+		'portfolio-style',
+        // get_stylesheet_uri is shortcut, as wp knows where to find it
+		get_stylesheet_uri(), 
+		array(),
+        // Some people may use date (09-01-25) or the version number; here I am telling wp to get version number of theme and output it here
+		wp_get_theme()->get( 'Version' ),
+		'all'
+	);
 
-    // 2. Theme stylesheet
-    wp_enqueue_style('portfolio-theme', get_stylesheet_uri());
+    // 2. Normalize CSS
+    wp_enqueue_style(
+        'normalize',
+        get_theme_file_uri('assets/css/normalize.css'),
+        array(),
+        '12.1.1'
+    );
 }
 add_action('wp_enqueue_scripts', 'portfoliotheme_enqueue_styles');
 
+// Register CPT
+require get_theme_file_path() . '/inc/cpt-taxonomies.php';
